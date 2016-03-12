2ae87ae546b619ab25f6338208f555655ae5ae37
Grant Harris <me@grantharr.is>
2016-03-12 17:09:58 +0000

    header management / navigation improvements, disable arrow key inputs / scrolling, dom improvements, js structure

--- scripts/main.js
+++ scripts/main.js
@@ -7,19 +7,69 @@ $(document).ready(function(){
 	  speed: 5000
 	});
 
-	// need to optimize this - fade in nav overlay / fade out content behind
-	// $("#burger").click(function(){
-	// 	$('#overlay').addClass('nav-on');
-	// 	$('#other').addClass('hide');
-	// });
 
-	// need to optimize this - fade out nav on out click / fade in content behind
-	// $("#overlay").click(function(){
-	// 	$('#overlay').removeClass('nav-on');
-	// 	$('#other').removeClass('hide');
-	// });
+  var menuLink = $('.menu__link');
+  var header = $('header');
+  var body = $('html');
 
+  menuLink.click(function(){
+
+    var f = !$(this).data("toggleFlag");
+
+    if (f){
+      header.animate({height: "100vh"}, 350, function(){
+      body.bind('touchmove scroll mousewheel', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
+      });
+    });
+
+    } else {
+      header.unbind();
+      body.unbind('touchmove scroll mousewheel');
+    }
+
+    $(this).data("toggleFlag", f);
+
+  });
 });
+
+
+// Disable arrow key scrolling
+$(document).keydown(function(e){
+  
+  if (e.keyCode == 37){ 
+    e.preventDefault();
+    e.stopPropagation();
+    return false;
+  }
+
+  if (e.keyCode == 38){ 
+    e.preventDefault();
+    e.stopPropagation();
+    return false;
+  }
+
+  if (e.keyCode == 39){ 
+    e.preventDefault();
+    e.stopPropagation();
+    return false;
+  }
+
+  if (e.keyCode == 40){ 
+    e.preventDefault();
+    e.stopPropagation();
+    return false;
+  }
+  
+});
+
+
+
+
+
+
 
 $(window).scroll(function(){
 
@@ -28,9 +78,6 @@ $(window).scroll(function(){
 
   var desiredWindowHeight = windowHeight * .6;  //since we use a height of 60vh, we take 60% of windowHeight for proper "push" animation
   var slideProgress = desiredWindowHeight - scrollTop;
-
-  var moveProgress = 1 - (windowHeight * (scrollTop / windowHeight));
-  var fadeProgress = 1 - (scrollTop / (0.5 * windowHeight));
 
   // negative zero
   if (scrollTop < 0){ 
@@ -50,14 +97,12 @@ $(window).scroll(function(){
     $('header').css('height', '100px');
   }
 
-  if(scrollTop > 380){
-  	$('#social').hide(); // clear from dom
-  }
 
-  if(scrollTop <= 380){
-  	$('#social').show(); // re-enter dom
-  }
+  // social link manipulation
+  var moveProgress = 1 - (windowHeight * (scrollTop / windowHeight));
+  var fadeProgress = 1 - (scrollTop / (0.5 * windowHeight));
 
+  //connect fade and slide
   $('.connect').css('opacity', fadeProgress);
 
   $('.connect').css('transform', 'translateY(' +  0.1 * moveProgress + 'px)');
